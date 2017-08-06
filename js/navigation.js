44d681644f36abfaf29314213c5ad5d5e5913205
Ilya Kushlianski <kushliansky@gmail.com>
2017-08-06 10:41:46 +0300

    This is the file with the code you need

--- js/navigation.js
+++ js/navigation.js
@@ -176,17 +176,18 @@
 		}
 		position = $(this).scrollTop();
 	});
-} )(jQuery);
 
-// make images with class ".extra-wide-img" appear full-screen
-$(document).ready(function(){
-	$("img").wrap("<figure class='extra-wide-img'></figure>");
-	$("figure.extra-wide-img").each(function(){
-		if ($(this).children("img").attr("width") > $(this).children("img").width()){
-			var containerMargins = $(".container").css("marginRight").replace(/[^-\d\.]/g, '');
-			var containerPadding = $(".container").css("paddingRight").replace(/[^-\d\.]/g, '');
-			var calc = $(".container").width() + (Number(containerMargins) + Number(containerPadding))*2;
-			$(this).css({"maxWidth": `${calc}px`, "margin": `1em -${Number(containerMargins) + Number(containerPadding)}px`});
-		}
+	// make images with class ".extra-wide-img" appear full-screen
+	$(document).ready(function(){
+		$("img").wrap("<figure class='extra-wide-img'></figure>");
+		$("figure.extra-wide-img").each(function(){
+			if ($(this).children("img").attr("width") > $(this).children("img").width()){
+				var containerMargins = $(".container").css("marginRight").replace(/[^-\d\.]/g, '');
+				var containerPadding = $(".container").css("paddingRight").replace(/[^-\d\.]/g, '');
+				var calc = $(".container").width() + (Number(containerMargins) + Number(containerPadding))*2;
+				$(this).css({"maxWidth": `${calc}px`, "margin": `1em -${Number(containerMargins) + Number(containerPadding)}px`});
+			}
+		});
 	});
-});
+
+} )(jQuery);
