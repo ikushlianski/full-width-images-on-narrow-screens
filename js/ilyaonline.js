a2a513358aa93ef2847268aac4fdfebb362341dc
Ilya Kushlianski <kushliansky@gmail.com>
2017-09-12 07:39:24 +0300

    Add behavior for background images for skills

--- js/ilyaonline.js
+++ js/ilyaonline.js
@@ -1,1 +1,27 @@
-
+// center the skills logo as background taking into account the size of the skills panel itself
+( function( $ ) {
+  $(document).ready(function(){
+    let thisSkillNowHeight = $('.this-skill-now').height();
+    let skillBackgroundWidth = $('.skillBackground').width();
+    let skillBackgroundHeight = $('.skillBackground').height();
+    if (thisSkillNowHeight >= skillBackgroundHeight) {
+      $('.skillBackground').css({
+        "left":`calc(50% - ${skillBackgroundWidth/2}px)`,
+        "left":`-webkit-calc(50% - ${skillBackgroundWidth/2}px)`,
+        "top":`calc(50% - ${skillBackgroundHeight/2}px)`,
+        "top":`-webkit-calc(50% - ${skillBackgroundHeight/2}px)`
+      });
+    } else {
+      $('.skillBackground')
+      .css({"height":`calc(${thisSkillNowHeight}px - 2rem)`});
+      skillBackgroundWidth = $('.skillBackground').width();
+      skillBackgroundHeight = $('.skillBackground').height();
+      $('.skillBackground').css({
+        "left":`calc(50% - ${skillBackgroundWidth/2}px)`,
+        "left":`-webkit-calc(50% - ${skillBackgroundWidth/2}px)`,
+        "top":`calc(50% - ${skillBackgroundHeight/2}px)`,
+        "top":`-webkit-calc(50% - ${skillBackgroundHeight/2}px)`
+      });
+    }
+  });
+})(jQuery);
